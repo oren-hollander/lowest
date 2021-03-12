be96b59c6ca13c0fce965d14ae45c0df3dfb4e59
Oren <ohollander@trigo.tech>
2021-03-12 12:06:00 +0200

    Fix keyBy tests

--- src/list/keyBy.test.ts
+++ src/list/keyBy.test.ts
@@ -2,10 +2,10 @@ import { keyBy } from './keyBy'
 
 describe('keyBy', () => {
   test('should return the first element', () => {
-    expect(keyBy([6.1, 4.2, 6.3], x => `${Math.floor(x)}`)).toEqual({ '4': [4.2], '6': [6.1, 6.3] })
+    expect(keyBy([6.1, 4.2, 6.3], x => `${Math.floor(x)}`)).toEqual({ '4': 4.2, '6': 6.3 })
   })
 
   test('should return the first element', () => {
-    expect(keyBy((x: number) => `${Math.floor(x)}`)([6.1, 4.2, 6.3])).toEqual({ '4': [4.2], '6': [6.1, 6.3] })
+    expect(keyBy((x: number) => `${Math.floor(x)}`)([6.1, 4.2, 6.3])).toEqual({ '4': 4.2, '6': 6.3 })
   })
 })
