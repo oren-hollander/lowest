74981db107fdb322a35a537b86a16556a35f9a3f
Oren <ohollander@trigo.tech>
2021-03-13 06:54:48 +0200

    filterDictionary docs

--- src/dictionary/filterDictionary/index.ts
+++ src/dictionary/filterDictionary/index.ts
@@ -5,8 +5,21 @@ import { curry2 } from '../../util/curry'
 
 const curried = curry2(filterDictionary)
 
+/**
+ * Returns a new dictionary containing entries who's values satisfies the predicate
+ * @param dict The [[Dictionary]]
+ * @param p The [[Predicate]]
+ * @return A new, filtered [[Dictionary]]
+ */
 function overloaded<T>(dict: Dictionary<T>, p: Predicate<T>): Dictionary<T>
+
+/**
+ * The curried version of the ```filteredDictionary```
+ * @param p The [[Predicate]]
+ * @return A function accepting a dictionary and returning a filtered dictionary
+ */
 function overloaded<T>(p: Predicate<T>): Fn<Dictionary<T>>
+
 function overloaded<T>(dict_p: Dictionary<T> | Predicate<T>, p?: Predicate<T>): Dictionary<T> | Fn<Dictionary<T>> {
   return curried(dict_p, p)
 }
