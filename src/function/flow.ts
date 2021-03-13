e982ba507fcd438e27a598511c7d2b7d5a6389bd
Oren <ohollander@trigo.tech>
2021-03-13 07:53:24 +0200

    docs

--- src/function/flow.ts
+++ src/function/flow.ts
@@ -1,31 +1,29 @@
 import { Fn } from './function'
 
-export function flow<T, TX1, TX2, TX3, TX4, TX5>(
-  values: T,
-  f1: Fn<T, TX1>,
-  tx2: Fn<TX1, TX2>,
-  tx3: Fn<TX2, TX3>,
-  tx4: Fn<TX3, TX4>,
-  tx5: Fn<TX4, TX5>
-): TX5
-export function flow<T, TX1, TX2, TX3, TX4>(
-  values: T,
-  tx1: Fn<T, TX1>,
-  tx2: Fn<TX1, TX2>,
-  tx3: Fn<TX2, TX3>,
-  tx4: Fn<TX3, TX4>
-): TX4
-export function flow<T, TX1, TX2, TX3>(values: T, f1: Fn<T, TX1>, tx2: Fn<TX1, TX2>, tx3: Fn<TX2, TX3>): TX3
-export function flow<T, TX1, TX2>(values: T, f1: Fn<T, TX1>, tx2: Fn<TX1, TX2>): TX2
-export function flow<T, TX1>(values: T, f1: Fn<T, TX1>): TX1
-
 /**
- * Composes several functions
+ * Composes and executes several functions
  * @param values The input to the composed function
- * @param fs Functions that are executed in order, each output is fed to the next function's input
+ * @param f1 Functions that are executed in order, each output is fed to the next function's input
+ * @param f2
+ * @param f3
+ * @param f4
+ * @param f5
  * @return the last function's output
  */
+export function flow<T, F1, F2, F3, F4, F5>(
+  values: T,
+  f1: Fn<T, F1>,
+  f2: Fn<F1, F2>,
+  f3: Fn<F2, F3>,
+  f4: Fn<F3, F4>,
+  f5: Fn<F4, F5>
+): F5
+export function flow<T, F1, F2, F3, F4>(values: T, f1: Fn<T, F1>, f2: Fn<F1, F2>, f3: Fn<F2, F3>, f4: Fn<F3, F4>): F4
+export function flow<T, F1, F2, F3>(values: T, f1: Fn<T, F1>, f2: Fn<F1, F2>, f3: Fn<F2, F3>): F3
+export function flow<T, F1, F2>(values: T, f1: Fn<T, F1>, f2: Fn<F1, F2>): F2
+export function flow<T, F1>(values: T, f1: Fn<T, F1>): F1
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function flow<T>(values: T, ...fs: ((...fns: unknown[]) => any)[]): unknown {
+export function flow<T>(values: T, ...fs: ((...fs: unknown[]) => any)[]): unknown {
   return fs.reduce((values, f) => f(values), values)
 }
