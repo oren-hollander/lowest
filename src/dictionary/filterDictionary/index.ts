f3be7d36f3bf12604fdac5706e6843ea7e9dd3af
Oren <ohollander@trigo.tech>
2021-03-13 07:02:31 +0200

    filterDictionary docs

--- src/dictionary/filterDictionary/index.ts
+++ src/dictionary/filterDictionary/index.ts
@@ -1,9 +1,9 @@
 import { Dictionary } from '../dictionary'
 import { Fn, Predicate } from '../../function'
-import { filterDictionary } from './filterDictionary'
+import { filterDictionary as impl } from './filterDictionary'
 import { curry2 } from '../../util/curry'
 
-const curried = curry2(filterDictionary)
+const curried = curry2(impl)
 
 /**
  * Returns a new dictionary containing entries who's values satisfies the predicate
@@ -11,17 +11,18 @@ const curried = curry2(filterDictionary)
  * @param p The [[Predicate]]
  * @return A new, filtered [[Dictionary]]
  */
-function overloaded<T>(dict: Dictionary<T>, p: Predicate<T>): Dictionary<T>
+export function filterDictionary<T>(dict: Dictionary<T>, p: Predicate<T>): Dictionary<T>
 
 /**
  * The curried version of the ```filteredDictionary```
  * @param p The [[Predicate]]
  * @return A function accepting a dictionary and returning a filtered dictionary
  */
-function overloaded<T>(p: Predicate<T>): Fn<Dictionary<T>>
+export function filterDictionary<T>(p: Predicate<T>): Fn<Dictionary<T>>
 
-function overloaded<T>(dict_p: Dictionary<T> | Predicate<T>, p?: Predicate<T>): Dictionary<T> | Fn<Dictionary<T>> {
+export function filterDictionary<T>(
+  dict_p: Dictionary<T> | Predicate<T>,
+  p?: Predicate<T>
+): Dictionary<T> | Fn<Dictionary<T>> {
   return curried(dict_p, p)
 }
-
-export { overloaded as filterDictionary }
